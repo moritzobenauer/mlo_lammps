74672b35047d4574fa9a7b7b0c2add1ee034efe1
moritzobenauer <moritz.obenauer@gmail.com>
2026-02-24 14:40:58 -0500

    Initial commit of my LAMMPS version

---
+++ doc/src/Developer.rst
@@ -0,0 +1,34 @@
+Information for Developers
+==========================
+
+This section describes the internal structure and basic algorithms
+of the LAMMPS code. This is a work in progress and additional
+information will be added incrementally depending on availability
+of time and requests from the LAMMPS user community.
+
+A discussion of software engineering methods applied to LAMMPS over time
+and a general outline of the design and maintenance approach by the
+LAMMPS developers can be found in the paper `LAMMPS: A Case Study For
+Applying Modern Software Engineering to an Established Research Software
+Package, <https://doi.org/10.5281/zenodo.17117558>` in USRSE'25
+conference proceedings.
+
+.. toctree::
+   :maxdepth: 1
+
+   Developer_org
+   Developer_code_design
+   Developer_parallel
+   Developer_atom
+   Developer_comm_ops
+   Developer_flow
+   Developer_write
+   Developer_notes
+   Developer_updating
+   Developer_plugins
+   Developer_unittest
+   Classes
+   Developer_platform
+   Developer_utils
+   Developer_internal
+   Developer_grid
---
+++ src/pair_lj_cut_mlo.h
@@ -0,0 +1,66 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   LAMMPS development team: developers@lammps.org
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
+   Contributing author: Moritz L. Obenauer @ Princeton University, 2026
+------------------------------------------------------------------------- */
+
+#ifdef PAIR_CLASS
+// clang-format off
+PairStyle(lj/cut/mlo,MLOPairLJCut)
+// clang-format on
+#else
+
+#ifndef LMP_PAIR_LJ_CUT_MLO_H
+#define LMP_PAIR_LJ_CUT_MLO_H
+
+#include "pair.h"
+
+namespace LAMMPS_NS {
+
+class MLOPairLJCut : public Pair {
+ public:
+  MLOPairLJCut(class LAMMPS *);
+  ~MLOPairLJCut() override;
+  void compute(int, int) override;
+  void settings(int, char **) override;
+  void coeff(int, char **) override;
+  double init_one(int, int) override;
+  void write_restart(FILE *) override;
+  void read_restart(FILE *) override;
+  void write_restart_settings(FILE *) override;
+  void read_restart_settings(FILE *) override;
+  void write_data(FILE *) override;
+  void write_data_all(FILE *) override;
+  // double single(int, int, int, int, double, double, double, double &) override;
+  void born_matrix(int, int, int, int, double, double, double, double &, double &) override;
+  void *extract(const char *, int &) override;
+
+//   void compute_inner() override;
+//   void compute_middle() override;
+//   void compute_outer(int, int) override;
+
+ protected:
+  double cut_global;
+  double **cut;
+  double **epsilon, **sigma;
+  double **lj1, **lj2, **lj3, **lj4, **offset, **LJ_MINIMUM;
+  double *cut_respa;
+
+  virtual void allocate();
+};
+
+}    // namespace LAMMPS_NS
+
+#endif
+#endif
