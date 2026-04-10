643a5d0aecf549900b6f68f580295d0b3533f447
Moritz Obenauer <132915491+moritzobenauer@users.noreply.github.com>
2026-04-10 14:09:56 -0400

    Add files via upload

---
+++ patches/fix_free_energy.cpp
@@ -0,0 +1,83 @@
+#include "fix_free_energy.h"
+#include "atom.h"
+#include "update.h"
+#include "respa.h"
+#include "error.h"
+#include "utils.h"
+#include <cmath>
+
+using namespace LAMMPS_NS;
+
+FixFreeEnergy::FixFreeEnergy(LAMMPS *_lmp, int narg, char **arg) : Fix(_lmp, narg, arg)
+{
+  // Expected syntax: fix ID group free_energy a b f
+  if (narg != 6) error->all(FLERR, "Illegal fix free_energy command. Expected: fix ID group free_energy a b f");
+
+  scalar_flag = 1;
+  extscalar = 1;
+  energy_global_flag = 1;
+  respa_level_support = 1;
+  ilevel_respa = 0;
+
+  coeff_a = utils::numeric(FLERR, arg[3], false, lmp); // z^4 coeff
+  coeff_b = utils::numeric(FLERR, arg[4], false, lmp); // z^2 coeff
+  coeff_f = utils::numeric(FLERR, arg[5], false, lmp); // z coeff
+}
+
+FixFreeEnergy::~FixFreeEnergy() {}
+
+int FixFreeEnergy::setmask()
+{
+  int mask = 0;
+  mask |= FixConst::POST_FORCE;
+  mask |= FixConst::POST_FORCE_RESPA;
+  return mask;
+}
+
+void FixFreeEnergy::init()
+{
+  if (utils::strmatch(update->integrate_style, "^respa")) {
+    ilevel_respa = (dynamic_cast<Respa *>(update->integrate))->nlevels - 1;
+  }
+}
+
+void FixFreeEnergy::setup(int vflag) { post_force(vflag); }
+
+void FixFreeEnergy::post_force(int /*vflag*/)
+{
+  double **x = atom->x;
+  double **f = atom->f;
+  int *mask = atom->mask;
+  int nlocal = atom->nlocal;
+
+  eflag = 0;
+  e_total = 0.0;
+
+  for (int i = 0; i < nlocal; i++) {
+    if (mask[i] & groupbit) {
+      double z = x[i][2];
+      
+      // Force Fz = -dV/dz = -(4*a*z^3 - 2*b*z + f)
+      // Based on your prompt: V = z^4 - b*z^2 + f*z (assuming a=1)
+      double f_z = -(4.0 * coeff_a * pow(z, 3) - 2.0 * coeff_b * z + coeff_f);
+      f[i][2] += f_z;
+
+      // Energy V = a*z^4 - b*z^2 + f*z
+      e_total += coeff_a * pow(z, 4) - coeff_b * pow(z, 2) + coeff_f * z;
+    }
+  }
+}
+
+void FixFreeEnergy::post_force_respa(int vflag, int ilevel, int /*iloop*/)
+{
+  if (ilevel == ilevel_respa) post_force(vflag);
+}
+
+double FixFreeEnergy::compute_scalar()
+{
+  if (eflag == 0) {
+    MPI_Allreduce(&e_total, &e_total_all, 1, MPI_DOUBLE, MPI_SUM, world);
+    eflag = 1;
+  }
+  return e_total_all;
+}
---
+++ patches/fix_free_energy.h
@@ -0,0 +1,35 @@
+#ifdef FIX_CLASS
+// This line MUST match the name you use in the input script
+FixStyle(free_energy, FixFreeEnergy); 
+#else
+
+#ifndef LMP_FIX_FREEENERGY_H
+#define LMP_FIX_FREEENERGY_H
+
+#include "fix.h"
+
+namespace LAMMPS_NS {
+
+
+class FixFreeEnergy : public Fix {
+ public:
+  FixFreeEnergy(class LAMMPS *, int, char **);
+  ~FixFreeEnergy() override;
+  int setmask() override;
+  void init() override;
+  void setup(int) override;
+  void post_force(int) override;
+  void post_force_respa(int, int, int) override;
+  double compute_scalar() override;
+
+ protected:
+  double coeff_a, coeff_b, coeff_f; // coefficients for z^4, z^2, z
+  int ilevel_respa;
+  int eflag;
+  double e_total, e_total_all;
+};
+
+}
+
+#endif
+#endif
